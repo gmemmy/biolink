8a5744fb4de6d917915f8a75842ee38a9d4c42ef
Emmanuel Atawodi <gmemmy22@gmail.com>
2025-07-14 22:30:09 +0100

    chore: migrate to eslint v9 and leverage flat config, clean up duplicate config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,10 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  extends: [
-    'eslint:recommended',
-    '@typescript-eslint/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
-    project: './tsconfig.base.json',
   },
   env: {
     node: true,
@@ -25,6 +21,10 @@ module.exports = {
     'node_modules/',
     'dist/',
     'build/',
+    'lib/',
+    'nitrogen/',
+    'ios/',
+    'android/',
     '*.js',
   ],
-}; 
+};
